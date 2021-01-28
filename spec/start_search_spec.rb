570535d7ddb8a5c16dc21a989f89ab479d68ee64
ZawadiSumba66 <rosesumba98@gmail.com>
2021-01-28 16:52:49 +0300

    Fix linters errors

--- spec/start_search_spec.rb
+++ spec/start_search_spec.rb
@@ -3,7 +3,7 @@ describe Iterate do
   let(:display) { described_class.new(@total, @page) }
   describe '#iterate' do
     context 'when the list is given with its keys and values present' do
-    # rubocop:disable  Metrics/BlockLength
+    # rubocop:disable Metrics/BlockLength
 
       it 'checks the various parts of the website and returns the job vacancies available' do
         list = {
@@ -45,7 +45,7 @@ describe Iterate do
         allow(display).to receive(:iterate).and_return(list)
         expect(list[:Address]).to be_nil
       end
-    # rubocop:enable  Metrics/BlockLength
+    # rubocop:enable Metrics/BlockLength
     end
   end
 
